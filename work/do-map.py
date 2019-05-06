cd072b565c906c47ac2ac602ad5e6a4482ae5727
WOXIANG <SunFeng@nuaa.edu.cn>
2019-05-06 20:42:04 +0800

    commit all

--- work/do-map.py
+++ work/do-map.py
@@ -52,7 +52,7 @@ print("************************************")
 def normalize(name):
   	name = name[0].upper() + name[1:].lower()
   	return name
-	
+
 L1 = ['adam', 'LISA', 'barT']
 L2 = list(map(normalize, L1))
 print(L2)
